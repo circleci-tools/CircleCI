808391f316f7ce6208978344b95013b3308bd1d3
Yuji Ueki <unhappychoice@gmail.com>
2019-05-26 23:41:40 +0900

    :+1: Create v1.1 specs

---
+++ src/test/kotlin/com/github/unhappychoice/circleci/MockCircleCIAPIClientV1_1.kt
@@ -0,0 +1,92 @@
+package com.github.unhappychoice.circleci
+
+import com.github.unhappychoice.circleci.request.TriggerNewBuildRequest
+import com.github.unhappychoice.circleci.request.TriggerNewBuildWithBranchRequest
+import com.github.unhappychoice.circleci.response.Artifact
+import com.github.unhappychoice.circleci.response.Build
+import com.github.unhappychoice.circleci.response.Project
+import com.github.unhappychoice.circleci.response.User
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import io.reactivex.Observable
+import java.io.File
+import java.lang.reflect.Type
+import java.util.*
+
+class MockCircleCIAPIClientV1_1: CircleCIAPIClientV1_1 {
+    override fun getMe(): Observable<User> {
+        return mockResponse("json/me.json", User::class.java)
+    }
+
+    override fun getProjects(): Observable<List<Project>> {
+        return mockResponse("json/projects.json", object: TypeToken<List<Project>>(){}.type)
+    }
+
+    override fun getProjectBuilds(vcsType: String, userName: String, project: String, offset: Int, limit: Int): Observable<List<Build>> {
+        return mockResponse("json/builds.json", object: TypeToken<List<Build>>(){}.type)
+    }
+
+    override fun getBranchBuilds(vcsType: String, userName: String, project: String, branch: String, offset: Int, limit: Int): Observable<List<Build>> {
+        return mockResponse("json/builds.json", object: TypeToken<List<Build>>(){}.type)
+    }
+
+    override fun getRecentBuilds(offset: Int, limit: Int): Observable<List<Build>> {
+        return mockResponse("json/builds.json", object: TypeToken<List<Build>>(){}.type)
+    }
+
+    override fun getBuild(vcsType: String, userName: String, project: String, buildNumber: Int): Observable<Build> {
+        return mockResponse("json/build.json", Build::class.java)
+    }
+
+    override fun getArtifacts(vcsType: String, userName: String, project: String, buildNumber: Int): Observable<List<Artifact>> {
+        return mockResponse("json/artifacts.json", object: TypeToken<List<Artifact>>(){}.type)
+    }
+
+    override fun followProject(vcsType: String, userName: String, project: String): Observable<Unit> {
+        return Observable.just(Unit)
+    }
+
+    override fun retryBuild(vcsType: String, userName: String, project: String, buildNumber: Int): Observable<Build> {
+        return mockResponse("json/build.json", Build::class.java)
+    }
+
+    override fun cancelBuild(vcsType: String, userName: String, project: String, buildNumber: Int): Observable<Build> {
+        return mockResponse("json/build.json", Build::class.java)
+    }
+
+    override fun addSSHUser(vcsType: String, userName: String, project: String, buildNumber: Int): Observable<Build> {
+        return mockResponse("json/build.json", Build::class.java)
+    }
+
+    override fun triggerNewBuild(vcsType: String, userName: String, project: String, request: TriggerNewBuildRequest): Observable<Build> {
+        return mockResponse("json/build.json", Build::class.java)
+    }
+
+    override fun triggerNewBuildWithBranch(vcsType: String, userName: String, project: String, branch: String, request: TriggerNewBuildWithBranchRequest): Observable<Build> {
+        return mockResponse("json/build.json", Build::class.java)
+    }
+
+    override fun deleteCache(vcsType: String, userName: String, project: String): Observable<Unit> {
+        return Observable.just(Unit)
+    }
+
+    private fun <T> mockResponse(fileName: String, type: Type): Observable<T> {
+        return Observable.just(converter().fromJson(readJson(fileName), type))
+    }
+
+    private fun converter() = GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        .create()
+
+    private fun readJson(fileName: String): String {
+        val file = File(MockCircleCIAPIClientV1::class.java.getResource(fileName).file)
+        val scanner = Scanner(file)
+        try {
+            return scanner.useDelimiter("\\A").next()
+        } finally {
+            scanner.close()
+        }
+    }
+}
