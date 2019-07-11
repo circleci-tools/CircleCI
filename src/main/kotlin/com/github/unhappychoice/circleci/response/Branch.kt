f784c58ac6cb9092330f850ed7a65b31c27bc478
Yuji Ueki <unhappychoice@gmail.com>
2019-07-12 01:06:22 +0900

    :recycle: Update specs

--- src/main/kotlin/com/github/unhappychoice/circleci/response/Branch.kt
+++
@@ -1,9 +0,0 @@
-package com.github.unhappychoice.circleci.response
-
-data class Branch(
-  val name: String,
-  val builds: Map<String, Any>,
-  val branchID: String,
-  val project: Project,
-  val pushers: Map<String, Any>
-)
